c7514eeb2c78602c44dbf4cff1b3a2ef49bfb4b7
Tatsuya KIMOTO <christian.dior.tatsuya@gmail.com>
2020-08-15 01:44:25 +0900

    CPP folder add

---
+++ Cpp/Nekodemo_cout01.cpp
@@ -0,0 +1,14 @@
+//
+//  Nekodemo_cout01.cpp
+//  Cpp_Training
+//
+//  Created by Tatsuya KIMOTO on 2020/08/15.
+//  Copyright © 2020 Tatsuya KIMOTO. All rights reserved.
+//
+
+#include "Nekodemo_cout01.hpp"
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ Cpp/Nekodemo_cout01.hpp
@@ -0,0 +1,14 @@
+//
+//  Nekodemo_cout01.hpp
+//  Cpp_Training
+//
+//  Created by Tatsuya KIMOTO on 2020/08/15.
+//  Copyright © 2020 Tatsuya KIMOTO. All rights reserved.
+//
+
+#ifndef Nekodemo_cout01_hpp
+#define Nekodemo_cout01_hpp
+
+#include <stdio.h>
+
+#endif /* Nekodemo_cout01_hpp */
