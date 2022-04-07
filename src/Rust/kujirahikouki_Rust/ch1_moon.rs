406869e2bd3c9d6223994c2580b2c8261bea841b
TatsuyaKIMOTO <christian.dior.tatsuya@gmail.com>
2022-04-07 23:04:31 +0900

    Rustのディレクトリ変更と新規ファイル追加

---
+++ src/Rust/kujirahikouki_Rust/ch1_moon.rs
@@ -0,0 +1,7 @@
+fn main(){
+    let moon = 384000.0;
+    let car = 80.0;
+    let btrain = 300.0;
+    println!("車で月まで{}日", moon/car/24.0);
+    println!("月まで新幹線で{}日", moon/btrain/24.0);
+}
---
+++ src/Rust/kujirahikouki_Rust/ch1_show_price.rs
@@ -0,0 +1,5 @@
+//Rustで数値を文字列を埋め込んで表示
+fn main() {
+    let banana = 300;
+    println!("バナナの値段={}円",banana)
+}
