c5851ccceac59638acc42742ebd343707343ae1b
TatsuyaKIMOTO <christian.dior.tatsuya@gmail.com>
2022-04-17 02:38:39 +0900

    python files added taikutsu python

---
+++ src/Python/OREILLY_Taikutsu_Python/taikutsu_test_p131_catnapping.py
@@ -0,0 +1,12 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+"""
+これは三連クオート
+のテストです
+"""
+#上記のダブルクオート三連は複数行にわたってコメントを書くときに頭とおしりにつけます
