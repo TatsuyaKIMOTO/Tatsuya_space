2034706f2ecc2edb13f95e37f14466ad9d6bbb12
Tatsuya KIMOTO <christian.dior.tatsuya@gmail.com>
2023-01-09 14:42:12 +0900

    20230109_Rust added

--- src/Rust/kujirahikouki_Rust/maze/target/debug/maze.d
+++ src/Rust/kujirahikouki_Rust/maze/target/debug/maze.d
@@ -1,1 +1,1 @@
-/home/travis/ドキュメント/git/Tatsuya_space/src/Rust/kujirahikouki_Rust/maze/target/debug/maze: /home/travis/ドキュメント/git/Tatsuya_space/src/Rust/kujirahikouki_Rust/maze/src/main.rs
+/Users/Tatsuya/書類/git/Tatsuya_space/src/Rust/kujirahikouki_Rust/maze/target/debug/maze: /Users/Tatsuya/書類/git/Tatsuya_space/src/Rust/kujirahikouki_Rust/maze/src/main.rs
