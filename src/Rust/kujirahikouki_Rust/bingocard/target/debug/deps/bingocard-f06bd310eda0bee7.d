c29e7be6ea0fd2df7534b50105ca09963243dfb1
Tatsuya KIMOTO <christian.dior.tatsuya@gmail.com>
2025-07-04 23:51:31 +0200

    Swift Lino added

--- src/Rust/kujirahikouki_Rust/bingocard/target/debug/deps/bingocard-f06bd310eda0bee7.d
+++
@@ -1,5 +0,0 @@
-/Users/Tatsuya/書類/git/Tatsuya_space/src/Rust/kujirahikouki_Rust/bingocard/target/debug/deps/bingocard-f06bd310eda0bee7: src/main.rs
-
-/Users/Tatsuya/書類/git/Tatsuya_space/src/Rust/kujirahikouki_Rust/bingocard/target/debug/deps/bingocard-f06bd310eda0bee7.d: src/main.rs
-
-src/main.rs:
